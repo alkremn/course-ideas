36433475725421ef062394a4cc6e117b59c93ce5
alexeykremnev <alkremn@gmail.com>
2018-08-26 22:00:02 -0600

    Initial commit

---
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -0,0 +1,42 @@
+package com.teamtreehouse.courses;
+
+
+
+import com.teamtreehouse.courses.model.CourseIdeaDAO;
+import com.teamtreehouse.courses.model.SimpleCourseIdeaDAO;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        get("/",(req, res) -> {
+            Map<String,String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+
+            return new ModelAndView(model, "index.hbs");
+            }, new HandlebarsTemplateEngine());
+
+        post("/sign-in",(req, res) -> {
+            Map<String, String> model = new HashMap<>();
+
+            String username = req.queryParams("username");
+            model.put("username", username);
+            res.cookie("username", username);
+
+            return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
+}
---
+++ src/main/java/com/teamtreehouse/courses/model/SimpleCourseIdeaDAO.java
@@ -0,0 +1,24 @@
+package com.teamtreehouse.courses.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleCourseIdeaDAO implements CourseIdeaDAO {
+
+    private List<CourseIdea> ideas;
+
+    public SimpleCourseIdeaDAO() {
+        this.ideas = new ArrayList<>();
+    }
+
+    @Override
+    public boolean add(CourseIdea courseIdea) {
+        return ideas.add(courseIdea);
+    }
+
+    @Override
+    public List<CourseIdea> findAll() {
+
+        return new ArrayList<>(ideas);
+    }
+}
