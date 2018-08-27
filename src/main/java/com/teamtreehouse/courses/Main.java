8d795d4412992a13998af8f7cc6bfa07c1370608
AlexK <alkremn@gmail.com>
2018-08-27 16:31:31 -0600

    Project finished

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -4,8 +4,10 @@ package com.teamtreehouse.courses;
 
 import com.teamtreehouse.courses.model.CourseIdea;
 import com.teamtreehouse.courses.model.CourseIdeaDAO;
+import com.teamtreehouse.courses.model.NotFoundException;
 import com.teamtreehouse.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 
@@ -16,16 +18,32 @@ import static spark.Spark.*;
 
 public class Main {
 
+    private static final String FLASH_MESSAGE_KEY = "flash_message";
+
     public static void main(String[] args) {
 
         staticFileLocation("/public");
 
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
+        before((req, res) -> {
+            if(req.cookie("username") != null){
+                req.attribute("username", req.cookie("username"));
+            }
+        });
+
+        before("/ideas", (req, res) -> {
+           if(req.attribute("username") == null){
+               setFlashMessage(req, "Whoops, please sign in first!");
+               res.redirect("/");
+               halt();
+           }
+        });
+
         get("/",(req, res) -> {
             Map<String,String> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
-
+            model.put("username", req.attribute("username"));
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "index.hbs");
             }, new HandlebarsTemplateEngine());
 
@@ -42,14 +60,63 @@ public class Main {
         get("/ideas", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("ideas",dao.findAll());
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/ideas", (req, res) -> {
-            dao.add(new CourseIdea(req.queryParams("title"),req.cookie("username")));
+            dao.add(new CourseIdea(req.queryParams("title"),req.attribute("username")));
             res.redirect("/ideas");
             return null;
 
         });
+
+        post("/ideas/:slug/vote", (req, res) -> {
+           CourseIdea idea = dao.findBySlug(req.params("slug"));
+           boolean added = idea.addVoter(req.attribute("username"));
+           if (added) {
+               setFlashMessage(req, "Thanks for your vote!");
+           }
+           else {
+               setFlashMessage(req, "You already voted!");
+           }
+           res.redirect("/ideas");
+           return null;
+        });
+
+        get("/ideas/:slug", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("idea",dao.findBySlug(req.params("slug")));
+            return modelAndView(model,"idea.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        exception(NotFoundException.class, (exc, req, res) ->{
+            res.status(404);
+            HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+            String html = engine.render(new ModelAndView(null, "not-found.hbs"));
+            res.body(html);
+        });
+    }
+
+    private static void setFlashMessage(Request req, String message) {
+        req.session().attribute(FLASH_MESSAGE_KEY, message);
+    }
+
+    private static String getFlashMessage(Request req){
+        if(req.session(false) == null){
+            return null;
+        }
+        if (!req.session().attributes().contains(FLASH_MESSAGE_KEY)) {
+
+            return null;
+        }
+        return (String) req.session().attribute(FLASH_MESSAGE_KEY);
+    }
+    private static String captureFlashMessage(Request req){
+        String message = getFlashMessage(req);
+        if(message != null){
+            req.session().removeAttribute(FLASH_MESSAGE_KEY);
+        }
+        return message;
     }
 }
